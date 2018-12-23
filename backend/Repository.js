620e51a4422d29a5af3ffda035697170c5f567c0
Phil Thomas <philip.m.thomas@icloud.com>
2018-12-23 10:43:35 -0500

    more refactoring

--- backend/Repository.js
+++ backend/Repository.js
@@ -3,21 +3,17 @@ const sql = require("mssql");
 const database = require("./Database");
 
 module.exports = {
-    executeQuery : function(res, query)
+    executeQuery : function(response, query)
     {
         const connectionPool = new sql.ConnectionPool(database.GetSettings());
 
         connectionPool.connect().then(pool =>
             pool.request().query(query)).then(result =>
         {
-            let rows = result.recordset;
-            res.setHeader('Access-Control-Allow-Origin','*');
-            res.status(200).json(rows);
-            sql.close();
-        }).catch(err =>
-        {
-            res.status(500).send({message : err});
-            sql.close();
-        });
+            response.setHeader('Access-Control-Allow-Origin','*');
+            response.status(200).json(result.recordset);
+        }).catch(error =>
+            response.status(500).send({message: error})).finally(() =>
+            sql.close());
     }
 };
